ad5edc4de1319ac299583c046651726b436a9e47
Christian Huang <christian@christians-imac.local>
2019-02-01 05:49:06 +0700

    Initial Commit

---
+++ CustomerRecords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomerRecords
+//
+//  Created by Christian  Huang on 01/02/19.
+//  Copyright © 2019 Christian Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
