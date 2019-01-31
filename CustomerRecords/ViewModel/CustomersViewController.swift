f6ac13238c540a6ecb701c3c3f98c78d70964279
Christian Huang <christian@christians-imac.local>
2019-02-01 05:56:28 +0700

    Add Folders for MVVM pattern

--- CustomerRecords/ViewModel/CustomersViewController.swift
+++ CustomerRecords/ViewModel/CustomersViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CustomersViewController.swift
 //  CustomerRecords
 //
 //  Created by Christian  Huang on 01/02/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CustomersViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
